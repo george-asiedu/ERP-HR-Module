f74c45feb4717ee3fcc9ee0549b0f0eed8f07195
George Asiedu <asiedug41@gmail.com>
2025-01-22 21:33:16 +0000

    typeORM configured in th application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { pgConfig } from '../dbConfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => pgConfig(configService),
     }),
   ],
   controllers: [AppController],

067d9cca571e345fc8ed4db0661e5783cdff0b79
George Asiedu <asiedug41@gmail.com>
2025-01-22 20:52:14 +0000

    swagger api documentation logic implemented

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Get the port from the environment using ConfigService
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('ERP System API Documentation')
+    .setDescription('API for managing the HR module in an ERP system')
+    .setVersion('1.0.0')
+    .addServer(`http://localhost:${port}/`, 'Local environment')
+    // .addTag('')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();

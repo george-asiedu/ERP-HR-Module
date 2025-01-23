511afd277592c98682bd7f8b9d732e1bf1a69c03
George Asiedu <asiedug41@gmail.com>
2025-01-23 02:13:39 +0000

    dbConfig file moved to src

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { pgConfig } from '../dbConfig';
+import { pgConfig } from './dbConfig';
 import { UsersModule } from './users/users.module';
 
 @Module({

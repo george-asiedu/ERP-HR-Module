a3eaf1b27ef62a0ccc1c27910364d287a0626892
George Asiedu <asiedug41@gmail.com>
2025-01-23 12:55:16 +0000

    synchronization updated to use set to false in production and true in development

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,5 +1,6 @@
 import { PostgresConnectionOptions} from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { ConfigService } from '@nestjs/config';
+import { User } from '../users/users.entity';
 
 export const pgConfig = (configService: ConfigService): PostgresConnectionOptions => ({
   type: 'postgres',
@@ -9,6 +10,8 @@ export const pgConfig = (configService: ConfigService): PostgresConnectionOption
   password: configService.get<string>('DB_PASSWORD', 'default_password'),
   database: configService.get<string>('DB_NAME', 'default_db'),
   url: configService.get<string>('DB_URL'),
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: configService.get<boolean>('DB_SYNC', true),
+  entities: [User],
+  synchronize: configService.get<string>('NODE_ENV') !== 'production'
+    ? configService.get<boolean>('DB_SYNC', true)
+    : false,
 });

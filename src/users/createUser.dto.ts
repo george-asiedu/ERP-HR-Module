de9623f606ca3e63bb85e41e7428542eb1ddd390
George Asiedu <asiedug41@gmail.com>
2025-01-23 00:18:21 +0000

    user dto created

---
+++ src/users/createUser.dto.ts
@@ -0,0 +1,39 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum UserRole {
+  Admin = 'Admin',
+  Employee = 'Employee'
+}
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'George Asiedu',
+    required: true
+  })
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @ApiProperty({
+    example: 'george.asiedu@gmail.com',
+    required: true
+  })
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @ApiProperty({
+    example: 'strongPass123',
+    required: true
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password!: string;
+
+  @ApiProperty({ example: "Admin|Employee", enum: UserRole })
+  @IsEnum(UserRole)
+  role!: UserRole;
+}

e502696232f5449f383caffe6939db8f512be7cc
George Asiedu <asiedug41@gmail.com>
2025-01-27 14:21:02 +0000

    updated to use constants value instead of strings

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { constants } from './utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(constants.globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
   const options = new DocumentBuilder()
-    .setTitle('ERP System API Documentation')
-    .setDescription('API for managing the HR module in an ERP system')
-    .setVersion('1.0.0')
-    .addServer(`http://localhost:${port}/`, 'Local environment')
-    .addServer('https://erp-system-hr-module.onrender.com', 'Production')
+    .setTitle(constants.swaggerDocsTitle)
+    .setDescription(constants.swaggerDocsDescription)
+    .setVersion(constants.swaggerDocsVersion)
+    .addServer(`${constants.localUrl}${port}/`, 'Local environment')
+    .addServer(constants.productionUrl, 'Production')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup(constants.swaggerDocsPath, app, document);
 
   app.enableCors();
   await app.listen(port);

dd25b6f947272eb60451ffb34d3e23bd220722c4
George Asiedu <asiedug41@gmail.com>
2025-01-27 14:20:28 +0000

    constants file created to hold magic string values

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+  swaggerDocsTitle: 'ERP System API Documentation',
+  swaggerDocsDescription: 'REST API for managing the HR module in an ERP system',
+  swaggerDocsVersion: '1.0.0',
+  swaggerDocsPath: 'api-docs',
+  productionUrl: 'https://erp-system-hr-module.onrender.com',
+  localUrl: 'http://localhost:',
+  globalPrefix: 'api',
+}

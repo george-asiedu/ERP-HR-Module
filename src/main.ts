ac4460e389453dbc486d4939575166be7f66f622
George Asiedu <asiedug41@gmail.com>
2025-01-30 02:04:45 +0000

    bearer auth added to swagger docs

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
     .setVersion(constants.swaggerDocsVersion)
     .addServer(`${constants.localUrl}${port}/`, 'Local environment')
     .addServer(constants.productionUrl, 'Production')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);

d199f7f9f7c8c6510cc05406054e89960b5123c9
George Asiedu <asiedug41@gmail.com>
2025-02-03 15:17:46 +0000

    email property removed

--- src/authentication/dto/resetPassword.dto.ts
+++ src/authentication/dto/resetPassword.dto.ts
@@ -1,13 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
-  @ApiProperty({ example: 'george.asiedu@gmail.com', required: true })
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
-  email!: string;
-
   @ApiProperty({ example: 'strongPass123', required: true })
   @IsString()
   @IsNotEmpty()

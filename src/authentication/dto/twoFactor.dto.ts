91ae1ceddbadad64ab3475b9c070c2b104329155
George Asiedu <asiedug41@gmail.com>
2025-02-03 15:01:50 +0000

    email property removed

--- src/authentication/dto/twoFactor.dto.ts
+++ src/authentication/dto/twoFactor.dto.ts
@@ -1,13 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class TwoFactorDto {
-  @ApiProperty({example: 'george.asiedu@gmail.com', required: true})
-  @IsEmail()
-  @IsString()
-  email!: string;
-
-  @ApiProperty({example: 787643, required: true})
+  @ApiProperty({example: '787643', required: true})
   @IsString()
   twoFactorCode!: string;
 }
